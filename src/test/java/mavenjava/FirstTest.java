80938b97be0b94d120e3a0e1dac6e79ca05101bf
mihaibordei <workbordeimihai@gmail.com>
2024-11-25 20:56:50 -0800

    checking CI again

--- src/test/java/mavenjava/FirstTest.java
+++ src/test/java/mavenjava/FirstTest.java
@@ -1,19 +1,27 @@
 package mavenjava;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class FirstTest extends BaseTest {
+public class FirstTest {
+
+    WebDriver driver;
 
     @Test
-    public void testStoreName() throws InterruptedException {
+    public void testStoreName() {
+
+
+        driver = new ChromeDriver();
+        driver.get("https://askomdch.com/");
 
         String websiteName = driver.findElement(By.xpath("//h1/a[@href='https://askomdch.com/']")).getText();
 
         Assert.assertEquals(websiteName, "AskOmDch");
 
-        Thread.sleep(3000);
+         driver.quit();
     }
 
 }
