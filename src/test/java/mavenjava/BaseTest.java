29fbb7bcea2701a42b509c3095281baf2709886a
mihaibordei <workbordeimihai@gmail.com>
2024-12-02 20:50:21 -0800

    commented @BeforClass

--- src/test/java/mavenjava/BaseTest.java
+++ src/test/java/mavenjava/BaseTest.java
@@ -1,11 +1,8 @@
 package mavenjava;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
