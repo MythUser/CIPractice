cab2c6a8f8b858894d50788cc55b10ec72e08d13
mihaibordei <workbordeimihai@gmail.com>
2024-12-02 20:28:03 -0800

    commented @BeforClass

--- src/test/java/mavenjava/BaseTest.java
+++ src/test/java/mavenjava/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -20,16 +21,21 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
     }
 
-//    @BeforeClass
-//    public void setUp() {
-//        // Устанавливаем драйвер с помощью WebDriverManager
-//        WebDriverManager.chromedriver().setup();
-//        // Получаем параметры Chrome из переменной окружения
-//        ChromeOptions options = new ChromeOptions();
-//        String chromeOpts = System.getenv("CHROME_OPTS");
-//        if (chromeOpts != null) {
-//            options.addArguments(chromeOpts.split(" "));
-//        }
-//        driver = new ChromeDriver(options);
-//    }
+    @BeforeClass
+    public void setUp() {
+        // Устанавливаем драйвер с помощью WebDriverManager
+        //WebDriverManager.chromedriver().setup();
+        // Получаем параметры Chrome из переменной окружения
+        ChromeOptions options = new ChromeOptions();
+        String chromeOpts = System.getenv("CHROME_OPTS");
+        if (chromeOpts != null) {
+            options.addArguments(chromeOpts.split(" "));
+        }
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public  void closeDriver(){
+        driver.quit();
+    }
 }
